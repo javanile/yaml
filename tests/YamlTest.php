13529e8e3ffbd345d297b1164dc35ae6e2211e67
francescobianco <info.francescobianco@gmail.com>
2017-02-25 18:02:33 +0100

    version 0.0.4

--- tests/YamlTest.php
+++ tests/YamlTest.php
@@ -7,26 +7,8 @@ use PHPUnit\Framework\TestCase;
  */
 final class YamlTest extends TestCase
 {
-    public function testCanBeCreatedFromValidEmailAddress()
+    public function testLoadOneItemFile()
     {
-        $this->assertInstanceOf(
-            Email::class,
-            Email::fromString('user@example.com')
-        );
-    }
 
-    public function testCannotBeCreatedFromInvalidEmailAddress()
-    {
-        $this->expectException(InvalidArgumentException::class);
-
-        Email::fromString('invalid');
-    }
-
-    public function testCanBeUsedAsString()
-    {
-        $this->assertEquals(
-            'user@example.com',
-            Email::fromString('user@example.com')
-        );
     }
 }
