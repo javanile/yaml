62273c98efc2880bcd99473d08b841e58b8f5070
francescobianco <info.francescobianco@gmail.com>
2017-02-25 10:13:02 +0100

    version 0.0.3

---
+++ tests/YamlTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers Email
+ */
+final class YamlTest extends TestCase
+{
+    public function testCanBeCreatedFromValidEmailAddress()
+    {
+        $this->assertInstanceOf(
+            Email::class,
+            Email::fromString('user@example.com')
+        );
+    }
+
+    public function testCannotBeCreatedFromInvalidEmailAddress()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        Email::fromString('invalid');
+    }
+
+    public function testCanBeUsedAsString()
+    {
+        $this->assertEquals(
+            'user@example.com',
+            Email::fromString('user@example.com')
+        );
+    }
+}
