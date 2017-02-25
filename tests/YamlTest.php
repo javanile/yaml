46c97c58ea2d622e03ca831e999b825a4fe90498
francescobianco <info.francescobianco@gmail.com>
2017-02-25 18:57:22 +0100

    version 0.0.4

--- tests/YamlTest.php
+++ tests/YamlTest.php
@@ -9,6 +9,8 @@ final class YamlTest extends TestCase
 {
     public function testLoadOneItemFile()
     {
+        $yaml = \Javanile\Yaml\Yaml::resolve(__DIR__.'/samples/one-item.yml');
 
+        $this->assertEquals($yaml, ['item' => 'one']);
     }
 }
